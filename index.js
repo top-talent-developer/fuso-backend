ad63de523b9b8fccacff095f980650498b5eba72
AkameYamate <OneTouch0525@gmail.com>
2021-12-07 19:54:51 +0800

    fix: router

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 8888;
 var cors = require('cors');
-
+console.log(port)
 
 // Use middlewares to set view engine and post json data to the server
 app.use(express.static('public'));
@@ -18,12 +18,12 @@ const frontEndPath = __dirname + "/../frontend/dist/fuse";
 app.use(express.static(frontEndPath));
 const frontEndPath2 = __dirname + "/../frontend_react/build";
 app.use(express.static(frontEndPath2));
-app.get('/v2/*', (req, res)=>{
+app.get('/v2', (req, res)=>{
     const html = fs.readFileSync(frontEndPath2 + '/index.html').toString('utf8')
     res.send(html);
 });
 
-app.get('*', (req, res)=>{
+app.get('/dashboard', (req, res)=>{
     const html = fs.readFileSync(frontEndPath + '/index.html').toString('utf8')
     res.send(html);
 });
